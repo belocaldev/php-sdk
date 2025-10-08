cfdb8b2487de075968c0d5b2f26d2cc2af55677c
bobredobre <m@bobredobre.dev>
2025-10-08 23:51:57 +0300

    fix request

--- src/BeLocalEngine.php
+++ src/BeLocalEngine.php
@@ -74,7 +74,7 @@ class BeLocalEngine
             }
 
             $batchRequestData[] = [
-                'request_id' => $requestId,
+                'requestId' => $requestId,
                 'payload' => $payload,
             ];
         }
